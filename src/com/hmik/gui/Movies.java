8536d23e0810bdffb3af73cf99596850a99f778b
hmik <mikulic.helena@yahoo.com>
2019-10-21 16:14:28 +0200

    Autocomplete feature added

--- src/com/hmik/gui/Movies.java
+++ src/com/hmik/gui/Movies.java
@@ -4,6 +4,7 @@ import com.hmik.QueryBuilder;
 import com.hmik.Movie;
 
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
 public class Movies {
     private JButton buttonSearch;
     public JPanel panelMain;
-    private JTextField queryInput;
+    private JComboBox queryInput;
     private JRadioButton radioAND;
     private JRadioButton radioOR;
     private JLabel queryLabel;
@@ -31,13 +32,18 @@ public class Movies {
     private JTextArea descriptionInput;
     private JLabel descriptionLabel;
     private JButton addMovieButton;
+    private JCheckBox autocomplete;
+    private JComboBox suggestionInput;
 
     public Movies() {
 
         descriptionInput.setLineWrap(true);
         sumaryInput.setLineWrap(true);
+        textArea1.setLineWrap(true);
+
+
         buttonSearch.addActionListener(actionEvent -> {
-            String q = queryInput.getText();
+            String q = ((JTextComponent)queryInput.getEditor().getEditorComponent()).getText();
             if( !q.equals("")){
                 List<Movie> movies = QueryBuilder.query(q, radioAND.isSelected());
                 textArea1.setText(QueryBuilder.getSQLText());
@@ -87,11 +93,46 @@ public class Movies {
             descriptionInput.setText("");
 
         });
-        queryInput.addKeyListener(new KeyAdapter() {
+        queryInput.getEditor().getEditorComponent().addKeyListener(new KeyAdapter() {
             @Override
             public void keyReleased(KeyEvent e) {
-                List<String> suggestions = QueryBuilder.getSuggestion(queryInput.getText());
+                if( autocomplete.isSelected()) {
+                    String q = ((JTextComponent) queryInput.getEditor().getEditorComponent()).getText();
+                    List<String> suggestions = QueryBuilder.getSuggestion(q);
+                    int index = 0;
+                    int size = queryInput.getItemCount();
+
+
+                    if (e.getKeyCode() == 13){
+                        queryInput.hidePopup();
+                        return;
+                    }
+                    if (e.getKeyCode() == 40) {
+                        queryInput.setSelectedIndex((index++) % size);
+                        ((JTextComponent) queryInput.getEditor().getEditorComponent()).setText(queryInput.getSelectedItem().toString());
+
+
+                    } else if (e.getKeyCode() == 38) {
+                        queryInput.setSelectedIndex((index--) % size);
+                        ((JTextComponent) queryInput.getEditor().getEditorComponent()).setText(queryInput.getSelectedItem().toString());
+
+                    }else if (e.getKeyCode() == 13) {
+                        queryInput.hidePopup();
+                    } else {
+                        if (!suggestions.isEmpty()) {
+                            queryInput.removeAllItems();
+                            queryInput.hidePopup();
+                            queryInput.addItem(q);
+                            for (String s : suggestions) {
+                                queryInput.addItem(s);
+                            }
+                            queryInput.showPopup();
+
+                        }
+                    }
+                }
             }
+
         });
     }
 }
